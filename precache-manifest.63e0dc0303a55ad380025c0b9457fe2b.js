fac7da8727519c666ac9f8c11e0bced0c9db5357
Igor Yunash <i.yunash@corp.vk.com>
2019-08-15 14:02:16 +0300

    Updates

---
+++ precache-manifest.63e0dc0303a55ad380025c0b9457fe2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74eec62170e2447ff3a6",
+    "url": "/vk_mini_apps_cache/static/js/runtime~main.de1b80eb.js"
+  },
+  {
+    "revision": "86fe2e2f24a8abbd082b",
+    "url": "/vk_mini_apps_cache/static/js/main.dbc7ef23.chunk.js"
+  },
+  {
+    "revision": "2300b46168ce6ed62cbf",
+    "url": "/vk_mini_apps_cache/static/js/2.fa6f36c4.chunk.js"
+  },
+  {
+    "revision": "2300b46168ce6ed62cbf",
+    "url": "/vk_mini_apps_cache/static/css/2.95afd777.chunk.css"
+  },
+  {
+    "revision": "efc34a3ee84baf079b9de576ec1061ec",
+    "url": "/vk_mini_apps_cache/index.html"
+  }
+];
